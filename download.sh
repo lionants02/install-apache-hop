fa6bc79911c44148de453d1dada8410409a27fb8
Max Thanachai Thongkum <singmax161@gmail.com>
2026-01-12 22:04:44 +0700

    Add resource limit notes for Docker and Jenkins
    Added comments for Docker and Jenkins resource limits.

--- download.sh
+++ download.sh
@@ -1,2 +1,10 @@
 curl https://dlcdn.apache.org/hop/2.16.0/apache-hop-client-2.16.0.zip -o apache-hop.zip
 unzip apache-hop.zip
+
+# Note cli docker or Jenkins limit memory 20g , cpu 16 core
+# docker run -m 20g --cpus=16 -it --rm \
+#     -v /your/local/project/folder:/files \
+#     -e HOP_FILE_PATH=/files/your-workflow.hwf \
+#     -e HOP_PROJECT_NAME=your-project \
+#     -e HOP_ENVIRONMENT_NAME=your-environment \
+#     apache/hop:2.16.0
